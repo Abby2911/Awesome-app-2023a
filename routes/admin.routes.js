edf7c964b46624eb78319504e6c9755b318da5cb
Abby2911 <reyesvianney448@gmail.com>
2023-05-23 14:36:25 +0000

    🔑 Class 23-05

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -7,14 +7,14 @@ import path from 'path';
 // Se crea una instancis del enrutador de express
 const router = Router();
 
-// GET /add-product
+// GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
     // Servimos el formulario
     console.log("🔊 Sirviendo formulario...");
     res.sendFile(path.resolve('views', 'add-product.html'));
 });
 
-// POST /add-product 
+// POST /admin/add-product 
 router.post('/add-product', (req, res) => {
     // Se realiza las extraccion del 
     // parametro dentro de la peticion 
